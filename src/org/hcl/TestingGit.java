26176ca8aaf3ebdc1bfda8b92d612eb70897708d
arunkumarmurugan <m.arunkumar.mak50@gmail.com>
2022-05-16 10:25:13 +0530

    Task Done

---
+++ src/org/hcl/TestingGit.java
@@ -0,0 +1,16 @@
+package org.hcl;
+
+public class TestingGit {
+private void javaClass() {
+	System.out.println("java class is started");
+}
+private void pythonClass() {
+	System.out.println("python class is not stated");
+}
+public static void main(String[] args) {
+	TestingGit t = new TestingGit();
+	
+	t.javaClass();
+	t.pythonClass();
+}
+}
