e003649bb7ca1ba8117ff906ccc95653b1b22a84
arunkumarmurugan <m.arunkumar.mak50@gmail.com>
2022-05-24 12:35:19 +0530

    Arun has updated some codes

--- src/org/hcl/TestingGit.java
+++ src/org/hcl/TestingGit.java
@@ -1,16 +1,21 @@
 package org.hcl;
 
 public class TestingGit {
-private void javaClass() {
-	System.out.println("java class is started");
-}
-private void pythonClass() {
-	System.out.println("python class is not stated");
-}
-public static void main(String[] args) {
-	TestingGit t = new TestingGit();
-	
-	t.javaClass();
-	t.pythonClass();
-}
+	public void javaClass() {
+		System.out.println("java class is started");
+	}
+	public void pythonClass() {
+		System.out.println("python class is not stated");
+	}
+	public void sql() {
+		System.out.println("sql class is not started");
+	}
+
+	public static void main(String[] args) {
+		TestingGit t = new TestingGit();
+
+		t.javaClass();
+		t.pythonClass();
+		t.sql();
+	}
 }
